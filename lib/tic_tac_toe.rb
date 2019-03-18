040245b4c1be70c4bf437623c9b4997aea0e256e
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-18 15:48:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,3 +14,14 @@ WIN_COMBINATIONS = [
   [0,4,8], # left diagonal win
   [2,4,6], # right diagonal win
 ]
+
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+end
