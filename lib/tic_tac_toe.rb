e50d01383df6edbd764a42686d8b1be3d25a18a9
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-18 15:53:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,15 @@ def position_taken?(board, index)
 def valid_move?(board, index)
 (board[index] ==  " " || board[index] == "" || board[index] == nil) && index.between?(0,8)
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
